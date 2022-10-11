c3e39541f84736adda829768065fbe776cb1d501
T Thaele <101729525+TrThaele@users.noreply.github.com>
2022-10-11 11:42:38 +0200

    Add files via upload
    I am trying to improve google speech especially on how it pronous african languages, e,g try translating english -> Sesotho,Zulu or Afrikaans, compare the results to how to pronouns wide spoken languages like French, Spanish, Hindi etc

---
+++ countries.js
@@ -0,0 +1,12 @@
+const countries = {
+    "ar-SA": "Arabic",
+    "en-GB": "English",
+    "es-ES": "Spanish",
+    "fr-FR": "French",
+    "pt-PT": "Portuguese",
+    "sn-ZW": "Shona",
+    "sw-SZ": "Swahili",
+    "tn-BW": "Tswana",
+    "xh-ZA": "Xhosa",
+    "zu-ZA": "Zulu"
+}
